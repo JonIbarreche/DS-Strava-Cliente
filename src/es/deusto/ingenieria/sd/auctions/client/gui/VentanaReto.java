fdd8599de3c16541470f787d26c4929d2e8f00db
evadelhoyo <eva.delhoyo@opendeusto.es>
2021-11-11 21:56:08 +0100

    ventanas

---
+++ src/es/deusto/ingenieria/sd/auctions/client/gui/VentanaReto.java
@@ -0,0 +1,199 @@
+package es.deusto.ingenieria.sd.auctions.client.gui;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import es.deusto.ingenieria.sd.auctions.client.gui.*;
+
+public class VentanaReto extends JFrame {
+	
+	protected JPanel panel,panelElige,panelNomReto,panelFechaIni,panelFechaFin,panelTod,panelDeporte,panelBotones;
+	protected Container cp;
+	protected JLabel labelElige,labelNomReto,labelFechaIni,labelFechaFin,labelTod,labelDeporte;
+	protected JTextField textoNomReto,textoFechaIni,textoFechaFin,textoTod;
+	protected ButtonGroup grupoDeporte;
+	protected JRadioButton rbMtb,rbCicl,rbRun;
+	protected JButton botonCrear,botonVolver;
+	protected JOptionPane creado;
+	protected JMenuBar menuBar;
+	protected JMenu menu;
+	protected JMenuItem menuItem;
+	
+	
+	public VentanaReto(String tod) {
+		
+		cp = this.getContentPane();
+		this.setTitle("CrearReto");
+		
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(6,1));
+		
+		menuBar = new JMenuBar();
+		menu = new JMenu("Más opciones");
+		
+		menuItem = new JMenuItem("Log out");
+		menuItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose(); //sierra ventana
+				
+			}
+		});
+		
+		//panel nombre reto
+		panelNomReto = new JPanel();
+		panelNomReto.setLayout(new FlowLayout());
+		
+		labelNomReto = new JLabel("Nombre del Reto:");
+		textoNomReto = new JTextField();
+		textoNomReto.setPreferredSize(new Dimension(200, 25));
+		
+		//panel fecha inicio
+		panelFechaIni = new JPanel();
+		panelFechaIni.setLayout(new FlowLayout());
+		
+		labelFechaIni = new JLabel("Fecha de Inicio (DD/MM/AAAA):");
+		textoFechaIni = new JTextField();
+		textoFechaIni.setPreferredSize(new Dimension(200, 25));
+		
+		//panel fecha de inicio
+		panelFechaFin = new JPanel();
+		panelFechaFin.setLayout(new FlowLayout());
+		
+		labelFechaFin = new JLabel("Fecha Fin (DD/MM/AAAA):");
+		textoFechaFin = new JTextField();
+		textoFechaFin.setPreferredSize(new Dimension(200, 25));
+		
+		//panel distancia
+		panelTod = new JPanel();
+		panelTod .setLayout(new FlowLayout());
+		
+		if(tod == "Distancia") {
+			labelTod = new JLabel("Distancia a Recorrer:");
+		} else if(tod == "Tiempo") {
+			labelTod = new JLabel("Tiempo Límite (h):");
+		}
+		textoTod = new JTextField();
+		textoTod.setPreferredSize(new Dimension(200, 25));
+		
+		//panel deporte
+		panelDeporte = new JPanel();
+		panelDeporte.setLayout(new FlowLayout());
+		
+		labelDeporte = new JLabel("Deporte:");
+		grupoDeporte = new ButtonGroup();
+		rbMtb = new JRadioButton("MTB");
+		rbCicl = new JRadioButton("Ciclismo");
+		rbRun = new JRadioButton("Running");
+		grupoDeporte.add(rbMtb);
+		grupoDeporte.add(rbCicl);
+		grupoDeporte.add(rbRun);
+		
+		//panelBotones
+		panelBotones = new JPanel();
+		panelBotones.setLayout(new FlowLayout());
+		//botones
+		botonVolver = new JButton("Volver");
+		botonVolver.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				VentanaPrincipal k = new VentanaPrincipal();
+				dispose();
+			}
+		});
+		
+		botonCrear = new JButton("Crear");
+		botonCrear.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String nombreReto = textoNomReto.getText();
+				String fechaIni = textoFechaIni.getText();
+				String fechaFin = textoFechaFin.getText();
+				int tiempo = 0;
+				float distancia = 0;
+				String deporte = "";
+				
+				if(rbCicl.isSelected() == true) {
+					deporte = "MTB";
+				} else if(rbMtb.isSelected() == true) {
+					deporte = "Ciclismo";
+				} else if(rbRun.isSelected() == true) {
+					deporte = "Running";
+				}
+				if(tod == "Distancia") {
+					distancia = Float.parseFloat(textoTod.getText());
+				tiempo = 0;
+				} else if(tod == "Tiempo") {
+					distancia = 0;
+					tiempo = Integer.parseInt(textoTod.getText());
+				}
+				
+				Reto nuevoReto = new Reto(nombreReto, fechaIni, fechaFin, distancia, tiempo, deporte);
+				
+				textoNomReto.setText("");
+				textoFechaIni.setText("");
+				textoFechaFin.setText("");
+				textoTod.setText("");
+				rbCicl.setSelected(false);
+				rbMtb.setSelected(false);
+				rbRun.setSelected(false);
+				
+				creado = new JOptionPane();
+				creado.showMessageDialog(null, "Nuevo Reto Creado Correctamente");
+
+			}
+		});
+		
+		
+		cp.add(panel);
+		setJMenuBar(menuBar);
+		menuBar.add(menu);
+		menu.add(menuItem);
+		
+		panel.add(panelNomReto);
+		panel.add(panelFechaIni);
+		panel.add(panelFechaFin);
+		panel.add(panelTod);
+		panel.add(panelDeporte);
+
+		panel.add(panelBotones);
+		
+		panelNomReto.add(labelNomReto);
+		panelNomReto.add(textoNomReto);
+
+		panelFechaIni.add(labelFechaIni);
+		panelFechaIni.add(textoFechaIni);
+		
+		panelFechaFin.add(labelFechaFin);
+		panelFechaFin.add(textoFechaFin);
+		
+		panelTod.add(labelTod);
+		panelTod.add(textoTod);
+		
+		panelDeporte.add(labelDeporte);
+		panelDeporte.add(rbCicl);
+		panelDeporte.add(rbMtb);
+		panelDeporte.add(rbRun);
+		
+		
+		
+		panelBotones.add(botonVolver);
+		panelBotones.add(botonCrear);
+		
+		
+		setVisible(true);
+		pack();
+		setSize(500,350);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+
+}
---
+++ src/es/deusto/ingenieria/sd/auctions/client/gui/VentanaSesion.java
@@ -0,0 +1,168 @@
+package es.deusto.ingenieria.sd.auctions.client.gui;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import javax.swing.*;
+
+import es.deusto.ingenieria.sd.auctions.client.gui.*;
+
+public class VentanaSesion extends JFrame{
+	protected JPanel panel,panelElige,panelTitulo,panelFechaIni,panelHoraIni,panelDuracion,panelDistancia,panelBotones;
+	protected Container cp;
+	protected JLabel labelElige,labelTitulo,labelFechaIni,labelHoraIni,labelDuracion,labelDistancia;
+	protected JTextField textoTitulo,textoFechaIni,textoHoraIni,textoDuracion,textoDistancia;
+	protected JButton botonCrear,botonVolver;
+	protected JOptionPane creado;
+	protected JMenuBar menuBar;
+	protected JMenu menu;
+	protected JMenuItem menuItem;
+	
+	
+	public VentanaSesion() {
+		
+		cp = this.getContentPane();
+		this.setTitle("CrearReto");
+		
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(6,1));
+		
+		menuBar = new JMenuBar();
+		menu = new JMenu("Más opciones");
+		
+		menuItem = new JMenuItem("Log out");
+		menuItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose(); //sierra ventana
+				
+			}
+		});
+		
+		//panel nombre reto
+		panelTitulo= new JPanel();
+		panelTitulo.setLayout(new FlowLayout());
+		
+		labelTitulo = new JLabel("Titulo de la Sesión:");
+		textoTitulo = new JTextField();
+		textoTitulo.setPreferredSize(new Dimension(200, 25));
+		
+		//panel fecha inicio
+		panelFechaIni = new JPanel();
+		panelFechaIni.setLayout(new FlowLayout());
+		
+		labelFechaIni = new JLabel("Fecha de Inicio (DD/MM/AAAA):");
+		textoFechaIni = new JTextField();
+		textoFechaIni.setPreferredSize(new Dimension(200, 25));
+		
+		//panel fecha de inicio
+		panelHoraIni = new JPanel();
+		panelHoraIni.setLayout(new FlowLayout());
+		
+		labelHoraIni = new JLabel("Hora Inicio (HH:MM):");
+		textoHoraIni = new JTextField();
+		textoHoraIni.setPreferredSize(new Dimension(200, 25));
+		
+		//panel distancia
+		panelDuracion = new JPanel();
+		panelDuracion .setLayout(new FlowLayout());
+		
+		labelDuracion = new JLabel("Duracion de la Sesión:");
+		textoDuracion = new JTextField();
+		textoDuracion.setPreferredSize(new Dimension(200, 25));
+		
+		//panel deporte
+		panelDistancia = new JPanel();
+		panelDistancia.setLayout(new FlowLayout());
+		
+		labelDistancia= new JLabel("Distancia a Recorrer:");
+		textoDistancia = new JTextField();
+		textoDistancia.setPreferredSize(new Dimension(200, 25));
+		
+		//panelBotones
+		panelBotones = new JPanel();
+		panelBotones.setLayout(new FlowLayout());
+		//botones
+		botonVolver = new JButton("Volver");
+		botonVolver.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				VentanaPrincipal k = new VentanaPrincipal();
+				dispose();
+			}
+		});
+		
+		botonCrear = new JButton("Crear");
+		botonCrear.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String titulo = textoTitulo.getText();
+				float distancia = Float.parseFloat(textoDistancia.getText());
+				String fechaIni = textoFechaIni.getText();
+				String horaIni = textoHoraIni.getText();
+				int Duracion = Integer.parseInt(textoDuracion.getText());
+			
+				Sesion nuevaSesion = new Sesion(titulo, distancia, fechaIni, horaIni, Duracion);
+				System.out.println(nuevaSesion.toString());
+				textoTitulo.setText("");
+				textoFechaIni.setText("");
+				textoHoraIni.setText("");
+				textoDistancia.setText("");
+				textoDuracion.setText("");
+				
+				creado = new JOptionPane();
+				creado.showMessageDialog(null, "Nueva Sesion Creada Correctamente");
+				
+
+			}
+		});
+		
+		
+		cp.add(panel);
+		setJMenuBar(menuBar);
+		menuBar.add(menu);
+		menu.add(menuItem);
+		
+		panel.add(panelTitulo);
+		panel.add(panelFechaIni);
+		panel.add(panelHoraIni);
+		panel.add(panelDistancia);
+		panel.add(panelDuracion);
+
+		panel.add(panelBotones);
+		
+		panelTitulo.add(labelTitulo);
+		panelTitulo.add(textoTitulo);
+
+		panelFechaIni.add(labelFechaIni);
+		panelFechaIni.add(textoFechaIni);
+		
+		panelHoraIni.add(labelHoraIni);
+		panelHoraIni.add(textoHoraIni);
+		
+		panelDuracion.add(labelDuracion);
+		panelDuracion.add(textoDuracion);
+		
+		panelDistancia.add(labelDistancia);
+		panelDistancia.add(textoDistancia);
+		
+		
+		panelBotones.add(botonVolver);
+		panelBotones.add(botonCrear);
+		
+		
+		setVisible(true);
+		pack();
+		setSize(500,350);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+}
