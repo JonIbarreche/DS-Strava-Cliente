5783d9fb3b445d05424bbc17e9b373b8bc5ebda1
evadelhoyo <eva.delhoyo@opendeusto.es>
2021-12-01 18:52:01 +0100

    funciona

--- src/es/deusto/ingenieria/sd/strava/client/gui/LoginWindow.java
+++ src/es/deusto/ingenieria/sd/strava/client/gui/LoginWindow.java
@@ -26,7 +26,6 @@ public class LoginWindow extends JFrame {
 	private JTextField txtMail;
 	private JTextField txtPassword;
 	protected ButtonGroup grupoLogin;
-	protected JRadioButton rbMail,rbFb,rbGoogle;
 	
 	
 
@@ -48,21 +47,7 @@ public class LoginWindow extends JFrame {
 		lblStravaCliente.setFont(new Font("Tahoma", Font.BOLD, 21));
 		lblStravaCliente.setBounds(188, 13, 229, 62);
 		contentPane.add(lblStravaCliente);
-		
-		grupoLogin = new ButtonGroup();
-		rbMail = new JRadioButton("Mail");
-		rbFb = new JRadioButton("Facebook");
-		rbGoogle = new JRadioButton("Google");
-		grupoLogin.add(rbMail);
-		grupoLogin.add(rbFb);
-		grupoLogin.add(rbGoogle);
-		contentPane.add(panelbotones);
-		panelbotones.add(rbMail);
-		panelbotones.add(rbFb);
-		panelbotones.add(rbGoogle);
-		
-		
-		
+	
 		txtMail = new JTextField();
 		txtMail.setText("Mail");
 		txtMail.setBounds(209, 103, 146, 28);
@@ -75,13 +60,46 @@ public class LoginWindow extends JFrame {
 		contentPane.add(txtPassword);
 		txtPassword.setColumns(10);
 		
+	
+		
+		JButton btnRegistrarse = new JButton("REGISTRARSE");
+		btnRegistrarse.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e) {
+				VentanaOpcion vo = new VentanaOpcion(logCtrl, regCtrl, retCtrl, sesCtrl);
+				vo.setVisible(true);
+				dispose();
+			}
+		});
+		btnRegistrarse.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnRegistrarse.setBounds(67, 284, 172, 46);
+		contentPane.add(btnRegistrarse);
+		
+		grupoLogin = new ButtonGroup();		
+		
+		
+		JRadioButton rdbtnMail = new JRadioButton("Mail");
+		rdbtnMail.setBounds(102, 217, 127, 25);
+		grupoLogin.add(rdbtnMail);
+		contentPane.add(rdbtnMail);
+		
+		JRadioButton rdbtnGoogle = new JRadioButton("Google");
+		rdbtnGoogle.setBounds(230, 217, 127, 25);
+		grupoLogin.add(rdbtnGoogle);
+		contentPane.add(rdbtnGoogle);
+		
+		JRadioButton rdbtnFacebook = new JRadioButton("Facebook");
+		rdbtnFacebook.setBounds(358, 217, 127, 25);
+		contentPane.add(rdbtnFacebook);
+		grupoLogin.add(rdbtnFacebook);
+		setVisible(true);
+		
 		JButton btnIniciarSesion = new JButton("INICIAR SESION");
 		btnIniciarSesion.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				boolean login;
-				if (rbFb.isSelected()) {
+				if (rdbtnFacebook.isSelected()) {
 					login = logCtrl.login(txtMail.getText(), "" , "Facebook");
-				} else if (rbGoogle.isSelected()) {
+				} else if (rdbtnGoogle.isSelected()) {
 					login = logCtrl.login(txtMail.getText(), "" , "Google");
 				} else {
 					login = logCtrl.login(txtMail.getText(), txtPassword.getText(), "Mail");
@@ -100,30 +118,5 @@ public class LoginWindow extends JFrame {
 		btnIniciarSesion.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnIniciarSesion.setBounds(337, 284, 172, 46);
 		contentPane.add(btnIniciarSesion);
-		
-		JButton btnRegistrarse = new JButton("REGISTRARSE");
-		btnRegistrarse.addActionListener(new ActionListener() {
-			 public void actionPerformed(ActionEvent e) {
-				VentanaOpcion vo = new VentanaOpcion(logCtrl, regCtrl, retCtrl, sesCtrl);
-				vo.setVisible(true);
-				dispose();
-			}
-		});
-		btnRegistrarse.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnRegistrarse.setBounds(67, 284, 172, 46);
-		contentPane.add(btnRegistrarse);
-		
-		JRadioButton rdbtnMail = new JRadioButton("Mail");
-		rdbtnMail.setBounds(102, 217, 127, 25);
-		contentPane.add(rdbtnMail);
-		
-		JRadioButton rdbtnGoogle = new JRadioButton("Google");
-		rdbtnGoogle.setBounds(230, 217, 127, 25);
-		contentPane.add(rdbtnGoogle);
-		
-		JRadioButton rdbtnFacebook = new JRadioButton("Facebook");
-		rdbtnFacebook.setBounds(358, 217, 127, 25);
-		contentPane.add(rdbtnFacebook);
-		setVisible(true);
 	}
 }
